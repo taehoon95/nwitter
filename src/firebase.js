25a6d66428e2260dee12399e1fb126ff41170a19
taehoon95 <leeth950304@gmail.com>
2021-10-03 13:59:55 +0900

    test

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5o7hjRTIjIIN8fYHtPF1MbMwGdeZ7sI4",
+    authDomain: "nwitter-39f66.firebaseapp.com",
+    projectId: "nwitter-39f66",
+    storageBucket: "nwitter-39f66.appspot.com",
+    messagingSenderId: "315610097618",
+    appId: "1:315610097618:web:29733cc09001fb272eca0b"
+  };
+  
+
+  export default firebase.initializeApp(firebaseConfig);
