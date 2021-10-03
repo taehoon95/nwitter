79c09503f21a6f7c6515f6858316765389a282c0
taehoon95 <leeth950304@gmail.com>
2021-10-03 14:46:25 +0900

    router에 prop으로 isLoggedIn을 넘겨준다.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <AppRouter />
+      <AppRouter isLoggedIn={isLoggedIn}/>
     </div>
   );
 }
